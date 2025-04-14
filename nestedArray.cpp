2f9640f2dac5ba9b3c51b47eab47894783b5c08f
AryaGGWP <arya.lampung.2016@gmail.com>
2025-04-14 18:21:21 +0700

    add library, sturct hero & skill serta datanya

---
+++ nestedArray.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Skill{
+    string skill1;
+    string skill2;
+    string skill3;
+};
+
+struct Hero{
+    string Nama;
+    string role;
+    string tipe;
+    Skill skill;
+};
